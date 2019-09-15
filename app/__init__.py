b5578ff9a442527d4b82ae9cf9a35c31ae347eaa
Robin <robinkaris7@gmail.com>
2019-09-15 21:18:52 +0300

    this commit makes changes to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,44 @@
 from flask import Flask
-from config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 
-app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.session_protection ='strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+simple = SimpleMDE()
 
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
 
+#initialize app
+def create_app(config_name):
+    app = Flask(__name__)
+    app.debug = True
 
-from .main import views
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    configure_uploads(app,photos)
+
+    return app
