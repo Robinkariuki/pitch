0ba01445544b122d6e2338ab666c6466ea85fad5
Robin <robinkaris7@gmail.com>
2019-09-17 11:54:32 +0300

    this commit modifies start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-export SECRET_KEY=1234
+set SECRET_KEY=1234
+set MAIL_USERNAME='robinkaris7@gmail.com'
+set DATABASE_URL ='postgresql+psycopg2://robin:zelda@localhost/pitch'
+python manage.py server
