bd142faf8b3991bba5b4ab359242bd4db5018f36
Robin <robinkaris7@gmail.com>
2019-09-18 15:34:10 +0300

    deploying

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://robin:zelda@localhost/pitch'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://robin:zelda@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS=True 
     CSRF_ENABLED = True
